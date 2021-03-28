3932f996dce1fb97fec9463d95fbef765319b37f
Lavender <endie.lavender@protonmail.com>
2021-03-27 20:42:29 -0700

    Change formatting

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
 var request = new XMLHttpRequest()
-request.open("GET", "https://cors-proxy.blaseball-reference.com/database/allTeams", true)
+request.open(
+    "GET",
+    "https://cors-proxy.blaseball-reference.com/database/allTeams",
+    true)
 request.send();
 
 request.onload = function() {
     var teams = JSON.parse(this.response)
     var totals = teams.map(team => team.lineup.length + team.rotation.length)
     // Get all player counts that there are from greatest to least
-    var playercounts = totals.slice().sort(
-        function (x, y) {
-            return y - x
-        }
-    ).filter(
-        function (item, pos, array) {
-            return !pos || item != array[pos - 1]
-        }
-    )
+    var playercounts = totals.slice().sort(function (x, y) {
+        return y - x
+    }).filter(function (item, pos, array) {
+        return !pos || item != array[pos - 1]
+    })
 
     // Now make headers & lists for all the player counts
     playercounts.forEach(function(item) {
-        document.body.insertAdjacentHTML("beforeend", "<h1>" 
-                                                      + item 
-                                                      + " players</h1><ul id='tlist"
-                                                      + item
-                                                      + "'></ul>")
+        document.body.insertAdjacentHTML("beforeend",
+            "<h1>"
+            + item 
+            + " players</h1><ul id='tlist"
+            + item
+            + "'></ul>")
     })
     // Put the teams in the lists
     teams.forEach(function(item, pos) {
-        document.getElementById("tlist" + totals[pos]).insertAdjacentHTML("beforeend", "<li>" 
-                                                                                       + item.fullName
-                                                                                       + " ("
-                                                                                       + item.lineup.length
-                                                                                       + " hitters, "
-                                                                                       + item.rotation.length
-                                                                                       + " pitchers)</li>")
+        document.getElementById("tlist" + totals[pos]).insertAdjacentHTML("beforeend", 
+            "<li>" 
+            + item.fullName
+            + " ("
+            + item.lineup.length
+            + " hitters, "
+            + item.rotation.length
+            + " pitchers)</li>")
     })
 
     document.getElementById("progress").remove()
