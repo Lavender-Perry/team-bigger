9529bafac6de56db4ac38ff3068377f21946a696
endie <endie@noreply.gitdab.com>
2021-04-01 01:21:15 +0000

    Minor changes to make code neater

--- main.js
+++ main.js
@@ -6,23 +6,25 @@ request.open(
 request.send();
 
 request.onload = function() {
+    let ending = x => (Math.abs(x) === 1 ? "" : "s")
+
     let teams = JSON.parse(this.response)
     let totals = teams.map(team => team.lineup.length + team.rotation.length)
     // Get all player counts that there are from greatest to least
     // & make headers & lists for them
     totals.slice().sort((x, y) => y - x)
-        .filter((item, pos, array) => !pos || item != array[pos - 1])
+        .filter((item, pos, array) => !pos || item !== array[pos - 1])
         .forEach(function(item) {
             document.body.insertAdjacentHTML("beforeend",
-                `<h1>${item} player${(item > 1 ? "s" : "")}</h1>
-                <ul id="tlist${item}"></ul>`)
+                `<h1>${item} player${ending(item)}</h1>
+                 <ul id="tlist${item}"></ul>`)
     })
     // Put the teams in the lists
     teams.forEach(function(item, pos) {
         document.getElementById("tlist" + totals[pos]).insertAdjacentHTML("beforeend", 
             `<li>${item.fullName} 
-            (${item.lineup.length} hitter${(item.lineup.length > 1 ? "s" : "")},
-             ${item.rotation.length} pitcher${(item.rotation.length > 1 ? "s" : "")})</li>`)
+            (${item.lineup.length} hitter${ending(item.lineup.length)},
+             ${item.rotation.length} pitcher${ending(item.rotation.length)})</li>`)
     })
 
     document.getElementById("progress").remove()
